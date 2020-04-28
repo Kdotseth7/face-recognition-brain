eb5409d4fc26fee638cc496c5a9ae932cd931c79
Kushagra Seth <kseth919@gmail.com>
2020-04-29 03:14:15 +0530

    Changing fetch url from lovalhost to remote

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends Component {
     };
 
     onRegister = () => {
-        fetch("http://localhost:3000/register", {
+        fetch("https://enigmatic-bastion-82399.herokuapp.com/register", {
             method: "post",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
